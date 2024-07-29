61cb0752c054d563580afa1a4f26ff8e3c8b86de
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-07-29 23:56:33 +0100

    outsourcing the form

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,13 +1,23 @@
-import FormSubmit from '@/components/form-submit';
+
 import { storePost } from '@/lib/posts';
 import { redirect } from 'next/navigation';
+import { useFormState } from "react-dom";
 
 export default function NewPostPage() {
+
   async function createPost(formData) {
     "use server";
     const title = formData.get('title');
     const image = formData.get('image');
     const content = formData.get('content');
+
+    let error = [];
+
+    if (!title || title.trim().length === 0) error.push("Title is required");
+    if (!image || image.trim().length === 0) error.push("Image is required");
+    if (!content || content.trim().length === 0) error.push("Content is required");
+
+    if (error.length > 0) return { error }; 
 
     storePost({
       imageUrl: '',
@@ -16,34 +26,9 @@ export default function NewPostPage() {
       userId: 1
     })
 
+
     redirect('/feed');
   }
 
-  return (
-    <>
-      <h1>Create a new post</h1>
-      <form action={createPost}>
-        <p className="form-control">
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
-        </p>
-        <p className="form-control">
-          <label htmlFor="image">Image URL</label>
-          <input
-            type="file"
-            accept="image/png, image/jpeg"
-            id="image"
-            name="image"
-          />
-        </p>
-        <p className="form-control">
-          <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows="5" />
-        </p>
-        <p className="form-actions">
-         <FormSubmit /> 
-        </p>
-      </form>
-    </>
-  );
+  
 }
