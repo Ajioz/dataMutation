0407fd9bf4197f085e1b62c9d9bc0f828407258f
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-07-29 23:15:19 +0100

    added formStatus

---
+++ components/form-submit.js
@@ -0,0 +1,20 @@
+'use client'
+
+import React from "react";
+import { useFormStatus } from "react-dom";
+
+const FormSubmit = () => {
+    const status = useFormStatus();
+    
+    if (status.pending) {
+        return <p>Creating Post...</p>
+    }
+  return (
+    <>
+      <button type="reset">Reset</button>
+      <button>Create Post</button>
+    </>
+  );
+};
+
+export default FormSubmit;
